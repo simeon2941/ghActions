8a19dc937cd480f45e9e30a79ca7574f670532f9
simeon2941 <75691955+simeon2941@users.noreply.github.com>
2023-04-22 22:08:51 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import openai
+def chat_with_gpt3():
+    messages = [{"role": "system", "content": "You are a helpful assistant."}]
+    
+    print("Type 'quit' to exit the conversation.")
+    while True:
+        user_input = input("You: ")   
+        if user_input.lower() == "quit":
+            break
+        messages.append({"role": "user", "content": user_input})       
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+        )
+        assistant_message = response.choices[0].message["content"]
+        print(f"Assistant: {assistant_message}")
+
+        messages.append({"role": "assistant", "content": assistant_message})
+
+if __name__ == "__main__":
+    chat_with_gpt3()
